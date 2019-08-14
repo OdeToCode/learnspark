84e442b6f7dc91da2c34d30378cbbd3ede48b7eb
K. Scott Allen <scott@OdeToCode.com>
2019-08-14 13:00:08 -0400

    dotnet and spark

---
+++ HelloSpark/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Spark.Sql;
+
+namespace HelloSpark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***********************Start!");
+            var spark = SparkSession.Builder().GetOrCreate();
+            var dataFrames = spark.Read().Json("people.json");
+            dataFrames.Show();
+        }
+    }
+}
